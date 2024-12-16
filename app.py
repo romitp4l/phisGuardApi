f0ac3dc600a0ac8917495680f109c311eec76578
ROMIT PAL <85341342+romitp4l@users.noreply.github.com>
2024-12-16 15:26:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,5 +25,6 @@ def analyze():
 
 if __name__ == '__main__':
    ## app.run(debug=True, host='0.0.0.0', port=int("5000"))
-   if __name__ == '__main__':
-    app.run(debug=True) #Remove host and port for Heroku
+  ## if __name__ == '__main__':
+  ##  app.run(debug=True) #Remove host and port for Heroku
+    app.run()
